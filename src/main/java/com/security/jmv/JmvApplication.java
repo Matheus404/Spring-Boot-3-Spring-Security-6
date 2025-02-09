18dbecc7696b60752fb3d771ea946d7c56a772d7
Vidal <jmvidal@stefanini.com>
2025-02-08 21:10:19 -0300

    first commit

---
+++ src/main/java/com/security/jmv/JmvApplication.java
@@ -0,0 +1,13 @@
+package com.security.jmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/jmv/JmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jmv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
