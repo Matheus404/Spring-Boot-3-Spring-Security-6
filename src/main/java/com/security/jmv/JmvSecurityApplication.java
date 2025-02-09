f023a8b32f4b89a5334f6586a3a3becdb7299164
Vidal <jmvidal@stefanini.com>
2025-02-08 21:47:57 -0300

    Add dependencies jjwt

--- src/main/java/com/security/jmv/JmvSecurityApplication.java
+++ src/main/java/com/security/jmv/JmvSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JmvApplication {
+public class JmvSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JmvApplication.class, args);
+		SpringApplication.run(JmvSecurityApplication.class, args);
 	}
 
 }
